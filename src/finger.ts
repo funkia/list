97ede5b2096bca7bd5edfbaadcb5f3f7d55ec1c4
paldepind <simonfv@gmail.com>
2017-05-27 14:54:39 +0200

    Add concat, still room for performance improvements.

--- src/finger.ts
+++ src/finger.ts
@@ -1,3 +1,5 @@
+// An affix is a list that can only have length 0 to 4. It is a
+// structure used internally in the finger tree.
 export class Affix<A> {
   constructor(
     public size: number,
@@ -6,22 +8,22 @@ export class Affix<A> {
     public b?: A,
     public c?: A,
     public d?: A
-  ) { };
+  ) { }
   toArray(): A[] {
     switch (this.len) {
-    case 0: return [];
-    case 1: return [this.a];
-    case 2: return [this.a, this.b];
-    case 3: return [this.a, this.b, this.c];
-    case 4: return [this.a, this.b, this.c, this.d];
+      case 0: return [];
+      case 1: return [this.a];
+      case 2: return [this.a, this.b];
+      case 3: return [this.a, this.b, this.c];
+      default: return [this.a, this.b, this.c, this.d];
     }
   }
   get(idx: number): A {
     switch (idx) {
-    case 0: return this.a;
-    case 1: return this.b;
-    case 2: return this.c;
-    default: return this.d;
+      case 0: return this.a;
+      case 1: return this.b;
+      case 2: return this.c;
+      default: return this.d;
     }
   }
 }
@@ -32,19 +34,6 @@ function affixPrepend<A>(size: number, a: A, as: Affix<A>): Affix<A> {
   return new Affix(as.size + size, as.len + 1, a, as.a, as.b, as.c);
 }
 
-function flatten<A>(a: NNode<A>[]): A[] {
-  let array: A[] = [];
-  for (let i = 0; i < a.length; ++i) {
-    const e = a[i];
-    array.push(e.a);
-    array.push(e.b);
-    if (e.three === true) {
-      array.push(e.c);
-    }
-  }
-  return array;
-}
-
 // Node in a 2-3 tree
 // export type NNode<A> = [A, A] | [A, A, A];
 export class NNode<A> {
@@ -53,13 +42,13 @@ export class NNode<A> {
     public three: boolean, // true if the node has three elements
     public a: A,
     public b: A,
-    public c: A,
-  ) { };
+    public c?: A
+  ) { }
   get(idx: number): A {
     switch (idx) {
       case 0: return this.a;
       case 1: return this.b;
-      case 2: return this.c;
+      default: return this.c;
     }
   }
 }
@@ -71,7 +60,7 @@ export class FingerTree<A> {
     public prefix: Affix<A>,
     public deeper: FingerTree<NNode<A>>,
     public suffix: Affix<A>
-  ) { };
+  ) { }
 }
 
 export const nil = new FingerTree<any>(
@@ -93,7 +82,7 @@ export function nrPrepend<A>(depth: number, size: number, a: A, t: FingerTree<A>
   if (t.size === 0) {
     return deep(depth, size, new Affix(size, 1, a), nil, emptyAffix);
   } else {
-   return nrPrependDeep<A>(t.prefix, depth, t, size, a);
+    return nrPrependDeep<A>(t.prefix, depth, t, size, a);
   }
 }
 
@@ -139,8 +128,52 @@ export function size(t: FingerTree<any>): number {
   return t.size;
 }
 
+// Concat
+
+function nodes(suffix: Affix<any>, digit: Affix<NNode<any>>, prefix: Affix<any>): Affix<NNode<any>> {
+  const deep = digit !== emptyAffix;
+  const array = suffix.toArray().reverse().concat(digit.toArray(), prefix.toArray());
+  let a; let b; let c; let d;
+  let result = emptyAffix;
+  let left = array.length; let nrOfNodes = 0;
+  while (left > 4) {
+    const size = deep === true ? array[left - 3].size + array[left - 2].size + array[left - 1].size : 3;
+    result = affixPrepend(size, new NNode(size, true, array[left - 3], array[left - 2], array[left - 1]), result);
+    left -= 3;
+  }
+  if (left === 2) {
+    const size = deep === true ? array[0].size + array[1].size : 2;
+    return affixPrepend(size, new NNode(size, false, array[0], array[1]), result);
+  } else if (left === 3) {
+    const size = deep === true ? array[0].size + array[1].size + array[2].size : 3;
+    return affixPrepend(size, new NNode(size, true, array[0], array[1], array[2]), result);
+  } else {
+    const size1 = deep === true ? array[0].size + array[1].size : 2;
+    const size2 = deep === true ? array[2].size + array[3].size : 2;
+    return affixPrepend(size1, new NNode(size1, false, array[0], array[1]), affixPrepend(size2, new NNode(size2, false, array[2], array[3]), result));
+  }
+}
+
+function doConcat<A>(depth: number, t1: FingerTree<A>, digit: Affix<any>, t2: FingerTree<A>): FingerTree<A> {
+  if (t1 === nil) {
+    return affixFoldr<any, FingerTree<any>>((a, t) => nrPrepend(depth, a.size, a, t), t2, digit);
+  } else if (t2 === nil) {
+    return affixFoldl<any, FingerTree<any>>((t, a) => nrAppend(depth, a.size, a, t), t1, digit, 0);
+  } else {
+    return deep(
+      depth, t1.size + t2.size + digit.size, t1.prefix, doConcat(depth + 1, t1.deeper, nodes(t1.suffix, digit, t2.prefix), t2.deeper), t2.suffix
+    );
+  }
+}
+
+export function concat<A>(list1: FingerTree<A>, list2: FingerTree<A>): FingerTree<A> {
+  return doConcat(0, list1, emptyAffix, list2);
+}
+
+// Get
+
 function affixGet<A>(depth: number, idx: number, affix: Affix<any>): A {
-  const {len, size, a, b, c, d} = affix;
+  const { len, size, a, b, c, d } = affix;
   if (len === size) {
     return affix.get(idx);
   }
@@ -159,7 +192,7 @@ function affixGet<A>(depth: number, idx: number, affix: Affix<any>): A {
 }
 
 function affixGetRev<A>(depth: number, idx: number, affix: Affix<any>): A {
-  const {len, size, a, b, c, d} = affix;
+  const { len, size, a, b, c, d } = affix;
   if (len === size) {
     return affix.get(len - 1 - idx);
   }
@@ -202,7 +235,7 @@ function nodeGet<A>(depth: number, idx: number, node: NNode<any>): A {
 }
 
 export function get<A>(idx: number, tree: FingerTree<A>): A {
-  let {size, prefix} = tree;
+  let { size, prefix } = tree;
   if (size === 0) {
     return undefined;
   }
@@ -215,7 +248,7 @@ export function get<A>(idx: number, tree: FingerTree<A>): A {
     prefSize = prefix.size;
     deepSize = prefSize + tree.deeper.size;
   }
-  const {depth} = tree;
+  const { depth } = tree;
   if (idx < prefSize) {
     return affixGet<A>(depth, idx, prefix);
   } else {
@@ -225,33 +258,43 @@ export function get<A>(idx: number, tree: FingerTree<A>): A {
 
 // Fold
 
-function nodeFoldLeft<A, B>(f: (b: B, a: A) => B, initial: B, node: Affix<any>, depth: number): B {
+function nodeFoldl<A, B>(f: (b: B, a: A) => B, initial: B, node: Affix<any>, depth: number): B {
   if (depth === 1) {
     return f(f(f(initial, node.a), node.b), node.c);
   } else {
-    const foldedA = nodeFoldLeft(f, initial, node.a, depth - 1);
-    const foldedB = nodeFoldLeft(f, foldedA, node.b, depth - 1);
-    const foldedC = nodeFoldLeft(f, foldedB, node.c, depth - 1);
+    const foldedA = nodeFoldl(f, initial, node.a, depth - 1);
+    const foldedB = nodeFoldl(f, foldedA, node.b, depth - 1);
+    const foldedC = nodeFoldl(f, foldedB, node.c, depth - 1);
     return foldedC;
+  }
+}
+
+function affixFoldr<A, B>(f: (a: A, b: B) => B, initial: B, affix: Affix<any>): B {
+  switch (affix.len) {
+    case 0: return initial;
+    case 1: return f(affix.a, initial);
+    case 2: return f(affix.a, f(affix.b, initial));
+    case 3: return f(affix.a, f(affix.b, f(affix.c, initial)));
+    default: return f(affix.a, f(affix.b, f(affix.c, f(affix.d, initial))));
   }
 }
 
 function affixFoldl<A, B>(f: (b: B, a: A) => B, initial: B, affix: Affix<any>, depth: number): B {
   if (depth === 0) {
     switch (affix.len) {
-    case 0: return initial;
-    case 1: return f(initial, affix.a);
-    case 2: return f(f(initial, affix.a), affix.b);
-    case 3: return f(f(f(initial, affix.a), affix.b), affix.c);
-    default: return f(f(f(f(initial, affix.a), affix.b), affix.c), affix.d);
+      case 0: return initial;
+      case 1: return f(initial, affix.a);
+      case 2: return f(f(initial, affix.a), affix.b);
+      case 3: return f(f(f(initial, affix.a), affix.b), affix.c);
+      default: return f(f(f(f(initial, affix.a), affix.b), affix.c), affix.d);
     }
   } else {
     switch (affix.len) {
-    case 0: return initial;
-    case 1: return nodeFoldLeft(f, initial, affix.a, depth);
-    case 2: return nodeFoldLeft(f, nodeFoldLeft(f, initial, affix.a, depth), affix.b, depth);
-    case 3: return nodeFoldLeft(f, nodeFoldLeft(f, nodeFoldLeft(f, initial, affix.a, depth), affix.b, depth), affix.c, depth);
-    default: return nodeFoldLeft(f, nodeFoldLeft(f, nodeFoldLeft(f, nodeFoldLeft(f, initial, affix.a, depth), affix.b, depth), affix.c, depth), affix.d, depth);
+      case 0: return initial;
+      case 1: return nodeFoldl(f, initial, affix.a, depth);
+      case 2: return nodeFoldl(f, nodeFoldl(f, initial, affix.a, depth), affix.b, depth);
+      case 3: return nodeFoldl(f, nodeFoldl(f, nodeFoldl(f, initial, affix.a, depth), affix.b, depth), affix.c, depth);
+      default: return nodeFoldl(f, nodeFoldl(f, nodeFoldl(f, nodeFoldl(f, initial, affix.a, depth), affix.b, depth), affix.c, depth), affix.d, depth);
     }
   }
 }
@@ -259,25 +302,25 @@ function affixFoldl<A, B>(f: (b: B, a: A) => B, initial: B, affix: Affix<any>, d
 function affixFoldlRev<A, B>(f: (b: B, a: A) => B, initial: B, affix: Affix<any>, depth: number): B {
   if (depth === 0) {
     switch (affix.len) {
-    case 0: return initial;
-    case 1: return f(initial, affix.a);
-    case 2: return f(f(initial, affix.b), affix.a);
-    case 3: return f(f(f(initial, affix.c), affix.b), affix.a);
-    default: return f(f(f(f(initial, affix.d), affix.c), affix.b), affix.a);
+      case 0: return initial;
+      case 1: return f(initial, affix.a);
+      case 2: return f(f(initial, affix.b), affix.a);
+      case 3: return f(f(f(initial, affix.c), affix.b), affix.a);
+      default: return f(f(f(f(initial, affix.d), affix.c), affix.b), affix.a);
     }
   } else {
     switch (affix.len) {
-    case 0: return initial;
-    case 1: return nodeFoldLeft(f, initial, affix.a, depth);
-    case 2: return nodeFoldLeft(f, nodeFoldLeft(f, initial, affix.b, depth), affix.a, depth);
-    case 3: return nodeFoldLeft(f, nodeFoldLeft(f, nodeFoldLeft(f, initial, affix.c, depth), affix.b, depth), affix.a, depth);
-    default: return nodeFoldLeft(f, nodeFoldLeft(f, nodeFoldLeft(f, nodeFoldLeft(f, initial, affix.d, depth), affix.c, depth), affix.b, depth), affix.a, depth);
+      case 0: return initial;
+      case 1: return nodeFoldl(f, initial, affix.a, depth);
+      case 2: return nodeFoldl(f, nodeFoldl(f, initial, affix.b, depth), affix.a, depth);
+      case 3: return nodeFoldl(f, nodeFoldl(f, nodeFoldl(f, initial, affix.c, depth), affix.b, depth), affix.a, depth);
+      default: return nodeFoldl(f, nodeFoldl(f, nodeFoldl(f, nodeFoldl(f, initial, affix.d, depth), affix.c, depth), affix.b, depth), affix.a, depth);
     }
   }
 }
 
 export function foldl<A, B>(f: (b: B, a: A) => B, initial: B, list: FingerTree<A>): B {
-  const {size, prefix, deeper, suffix, depth} = list;
+  const { size, prefix, deeper, suffix, depth } = list;
   if (size === 0) {
     return initial;
   } else {
@@ -285,6 +328,19 @@ export function foldl<A, B>(f: (b: B, a: A) => B, initial: B, list: FingerTree<A
     const foldedMiddle = deeper === undefined ? foldedSuffix : foldl<A, B>(f, foldedSuffix, <any>deeper);
     return prefix === undefined ? foldedMiddle : affixFoldl(f, foldedMiddle, prefix, depth);
   }
+}
+
+function flatten<A>(a: NNode<A>[]): A[] {
+  let array: A[] = [];
+  for (let i = 0; i < a.length; ++i) {
+    const e = a[i];
+    array.push(e.a);
+    array.push(e.b);
+    if (e.three === true) {
+      array.push(e.c);
+    }
+  }
+  return array;
 }
 
 export function toArray<A>(t: FingerTree<A>): A[] {
--- test/index.ts
+++ test/index.ts
@@ -1,11 +1,33 @@
-/// <reference path="./../typings/index.d.ts" />
-import {assert} from "chai";
-import * as util from "util";
+import { assert } from "chai";
 
-import {FingerTree, prepend, append, toArray, size, get, nil, foldl} from "../src/finger";
+import { FingerTree, prepend, append, toArray, size, get, nil, foldl, concat } from "../src/finger";
+
+function randomNumber(max: number): number {
+  return Math.floor(Math.random() * max);
+}
+
+function randomBool(): boolean {
+  return Math.random() > 0.5;
+}
 
 function subtract(n: number, m: number): number {
   return n - m;
+}
+
+function createList(from: number, n: number): FingerTree<number> {
+  let list: FingerTree<number> = nil;
+  for (let i = from; i < from + n; ++i) {
+    list = append(i, list);
+  }
+  return list;
+}
+
+function createArray(from: number, n: number): number[] {
+  let array = [];
+  for (let i = from; i < from + n; ++i) {
+    array.push(i);
+  }
+  return array;
 }
 
 describe("Finger tree", () => {
@@ -38,34 +60,98 @@ describe("Finger tree", () => {
     const list = prepend(5, prepend(4, prepend(3, prepend(2, prepend(1, prepend(0, nil))))));
     assert.deepEqual(size(list), 6);
   });
-  it("can index", () => {
-    const list = prepend(5, prepend(4, prepend(3, prepend(2, prepend(1, prepend(0, nil))))));
-    assert.deepEqual(get(0, list), 5);
-    assert.deepEqual(get(2, list), 3);
+  describe("concat", () => {
+    it("empty list to list", () => {
+      const list = createList(0, 10);
+      const concatenated = concat(list, nil);
+      assert.deepEqual(toArray(concatenated), createArray(0, 10));
+      const concatenated2 = concat(nil, list);
+      assert.deepEqual(toArray(concatenated2), createArray(0, 10));
+    });
+    it("singleton list to list", () => {
+      const list = createList(0, 10);
+      const singleton = prepend(10, nil);
+
+      const concatenated = concat(list, singleton);
+      const array = createArray(0, 11);
+      assert.deepEqual(toArray(concatenated), array);
+
+      const concatenated2 = concat(singleton, list);
+      const array2 = createArray(0, 10);
+      array2.unshift(10);
+      assert.deepEqual(toArray(concatenated2), array2);
+    });
+    it("can index into concatenated lists", () => {
+      const list1 = createList(0, 20);
+      const list2 = createList(20, 20);
+      const concatenated = concat(list1, list2);
+      for (let i = 0; i < 40; ++i) {
+        assert.strictEqual(get(i, concatenated), i);
+      }
+    });
+    it("two lists", () => {
+      let times = 100;
+      while (--times > 0) {
+        const array1 = [];
+        const array2 = [];
+        const n = randomNumber(300);
+        const m = randomNumber(300);
+        let list1 = nil;
+        let list2 = nil;
+        for (let i = 0; i < n; ++i) {
+          if (randomBool()) {
+            list1 = append(i, list1);
+            array1.push(i);
+          } else {
+            list1 = prepend(i, list1);
+            array1.unshift(i);
+          }
+        }
+        for (let i = 0; i < m; ++i) {
+          if (randomBool()) {
+            list2 = append(i, list2);
+            array2.push(i);
+          } else {
+            list2 = prepend(i, list2);
+            array2.unshift(i);
+          }
+        }
+        const concatenated = concat(list1, list2);
+        const array = array1.concat(array2);
+        assert.deepEqual(toArray(concatenated), array);
+      }
+    });
   });
-  it("can large index", () => {
-    const n = 10000;
-    let list: FingerTree<number> = nil;
-    for (let i = 0; i < n; ++i) {
-      list = append(i, list);
-    }
-    let arr: number[] = [];
-    for (let i = 0; i < n; ++i) {
-      arr.push(get(i, list));
-    }
-    assert.deepEqual(toArray(list), arr);
-  });
-  it("can large index into prepend built tree", () => {
-    const n = 10000;
-    let list: FingerTree<number> = nil;
-    for (let i = 0; i < n; ++i) {
-      list = prepend(i, list);
-    }
-    let arr: number[] = [];
-    for (let i = 0; i < n; ++i) {
-      arr.push(get(i, list));
-    }
-    assert.deepEqual(arr, toArray(list));
+  describe("indexing", () => {
+    it("can index", () => {
+      const list = prepend(5, prepend(4, prepend(3, prepend(2, prepend(1, prepend(0, nil))))));
+      assert.deepEqual(get(0, list), 5);
+      assert.deepEqual(get(2, list), 3);
+    });
+    it("can large index", () => {
+      const n = 10000;
+      let list: FingerTree<number> = nil;
+      for (let i = 0; i < n; ++i) {
+        list = append(i, list);
+      }
+      let arr: number[] = [];
+      for (let i = 0; i < n; ++i) {
+        arr.push(get(i, list));
+      }
+      assert.deepEqual(toArray(list), arr);
+    });
+    it("can large index into prepend built tree", () => {
+      const n = 10000;
+      let list: FingerTree<number> = nil;
+      for (let i = 0; i < n; ++i) {
+        list = prepend(i, list);
+      }
+      let arr: number[] = [];
+      for (let i = 0; i < n; ++i) {
+        arr.push(get(i, list));
+      }
+      assert.deepEqual(arr, toArray(list));
+    });
   });
   describe("folding", () => {
     describe("foldl", () => {
