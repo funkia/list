681d07b838393c518f1a44f91eb9372a555aae4e
Simon Friis Vindum <simonfv@gmail.com>
2018-02-26 17:58:58 +0100

    Make curried functions type-safe

--- src/curried.ts
+++ src/curried.ts
@@ -21,10 +21,15 @@ export {
   fromIterable
 } from "./index";
 
-function curry2(f: (a: any, b: any) => any): any {
+interface Curried2<A, B, R> {
+  (t1: A): (t2: B) => R;
+  (t1: A, t2: B): R;
+}
+
+function curry2(f: Function): any {
   return function curried(a: any, b: any): any {
     return arguments.length === 2 ? f(a, b) : (b: any) => f(a, b);
-  } as any;
+  };
 }
 
 function curry3(f: (a: any, b: any, c: any) => any): any {
@@ -33,68 +38,166 @@ function curry3(f: (a: any, b: any, c: any) => any): any {
       case 3:
         return f(a, b, c);
       case 2:
-        return curry2((b: any, c: any) => f(a, b, c));
+        return (c: any) => f(a, b, c);
       default:
         // Assume 1
-        return (c: any) => f(a, b, c);
+        return curry2((b: any, c: any) => f(a, b, c));
     }
   };
 }
 
 // Arity 2
+
 export const prepend: typeof L.prepend &
   (<A>(value: A) => (l: List<A>) => List<A>) = curry2(L.prepend);
-export const append: typeof L.append &
-  (<A>(value: A) => (l: List<A>) => List<A>) = curry2(L.append);
+
+export const append: typeof prepend = curry2(L.append);
+
 export const pair: typeof L.pair &
   (<A>(first: A) => (second: A) => List<A>) = curry2(L.pair);
-export const repeat: typeof L.repeat = curry2(L.repeat);
-export const times: typeof L.times = curry2(L.times);
-export const nth: typeof L.nth = curry2(L.nth);
+
+export const repeat: typeof L.repeat &
+  (<A>(value: A) => (times: number) => List<A>) = curry2(L.repeat);
+
+export const times: typeof L.times &
+  (<A>(func: (index: number) => A) => (times: number) => List<A>) = curry2(
+  L.times
+);
+
+export const nth: typeof L.nth &
+  ((index: number) => <A>(l: List<A>) => A | undefined) = curry2(L.nth);
 
 export const map: typeof L.map &
   (<A, B>(f: (a: A) => B) => (l: List<A>) => List<B>) = curry2(L.map);
 
-export const forEach: typeof L.forEach = curry2(L.forEach);
-export const pluck: typeof L.pluck = curry2(L.pluck as (<A>(
-  key: string,
-  l: L.List<A>
-) => A));
-export const range: typeof L.range = curry2(L.range);
-export const filter: typeof L.filter = curry2(L.filter);
-export const reject: typeof L.reject = curry2(L.reject);
-export const partition: typeof L.partition = curry2(L.partition);
-export const join: typeof L.join = curry2(L.join);
-export const ap: typeof L.ap = curry2(L.ap);
-export const chain: typeof L.chain = curry2(L.chain);
-export const every: typeof L.every = curry2(L.every);
-export const all: typeof L.all = every;
-export const some: typeof L.some = curry2(L.some);
-export const any: typeof L.any = some;
-export const none: typeof L.none = curry2(L.none);
-export const find: typeof L.find = curry2(L.find);
-export const indexOf: typeof L.indexOf = curry2(L.indexOf);
-export const findIndex: typeof L.findIndex = curry2(L.findIndex);
-export const includes: typeof L.includes = curry2(L.includes);
-export const contains: typeof L.contains = includes;
-export const equals: typeof L.equals = curry2(L.equals);
-export const concat: typeof L.concat = curry2(L.concat);
-export const take: typeof L.take = curry2(L.take);
-export const takeWhile: typeof L.takeWhile = curry2(L.takeWhile);
-export const dropWhile: typeof L.dropWhile = curry2(L.dropWhile);
-export const takeLast: typeof L.takeLast = curry2(L.takeLast);
-export const splitAt: typeof L.splitAt = curry2(L.splitAt);
-export const drop: typeof L.drop = curry2(L.drop);
-export const dropLast: typeof L.dropLast = curry2(L.dropLast);
+export const forEach: typeof L.forEach &
+  (<A>(callback: (a: A) => void) => (l: List<A>) => void) = curry2(L.forEach);
+
+export const pluck: typeof L.pluck &
+  (<K extends string>(
+    key: K
+  ) => <C, B extends K & (keyof C)>(l: List<C>) => List<C[B]>) = curry2(
+  L.pluck
+);
+
+export const range: typeof L.range &
+  ((start: number) => (end: number) => List<number>) = curry2(L.range);
+
+export const filter: typeof L.filter &
+  (<A>(predicate: (a: A) => boolean) => (l: List<A>) => List<A>) = curry2(
+  L.filter
+);
+
+export const reject: typeof filter = curry2(L.reject);
+
+export const partition: typeof L.partition &
+  (<A>(predicate: (a: A) => boolean) => (l: List<A>) => List<List<A>>) = curry2(
+  L.partition
+);
+
+export const join: typeof L.join &
+  ((seperator: string) => (l: List<string>) => List<string>) = curry2(L.join);
+
+export const ap: typeof L.ap &
+  (<A, B>(listF: List<(a: A) => B>) => (l: List<A>) => List<B>) = curry2(L.ap);
+
+export const chain: typeof L.chain &
+  (<A, B>(f: (a: A) => List<B>) => (l: List<A>) => List<B>) = curry2(L.chain);
+
+export const every: typeof L.every &
+  (<A>(predicate: (a: A) => boolean) => (l: List<A>) => boolean) = curry2(
+  L.every
+);
+
+export const all: typeof every = curry2(L.all);
+
+export const some: typeof every = curry2(L.some);
+
+export const any: typeof every = curry2(L.any);
+
+export const none: typeof every = curry2(L.none);
+
+export const find: typeof L.find &
+  (<A>(predicate: (a: A) => boolean) => (l: List<A>) => A | undefined) = curry2(
+  L.find
+);
+
+export const indexOf: typeof L.indexOf &
+  (<A>(element: A) => (l: List<A>) => number) = curry2(L.indexOf);
+
+export const findIndex: typeof L.findIndex &
+  (<A>(predicate: (a: A) => boolean) => (l: List<A>) => number) = curry2(
+  L.findIndex
+);
+
+export const includes: typeof L.includes &
+  (<A>(element: A) => (l: List<A>) => number) = curry2(L.includes);
+
+export const contains = includes;
+
+export const equals: typeof L.equals &
+  (<A>(first: List<A>) => (second: List<A>) => boolean) = curry2(L.equals);
+
+export const concat: typeof L.concat &
+  (<A>(left: List<A>) => (right: List<A>) => List<A>) = curry2(L.concat);
+
+export const take: typeof L.take &
+  ((n: number) => <A>(l: List<A>) => List<A>) = curry2(L.take);
+
+export const takeLast: typeof take = curry2(L.takeLast);
+
+export const drop: typeof take = curry2(L.drop);
+
+export const dropLast: typeof take = curry2(L.dropLast);
+
+export const takeWhile: typeof filter = curry2(L.takeWhile);
+
+export const dropWhile: typeof filter = curry2(L.dropWhile);
+
+export const splitAt: typeof L.splitAt &
+  ((index: number) => <A>(l: List<A>) => [List<A>, List<A>]) = curry2(
+  L.splitAt
+);
 
 // Arity 3
-export const foldl = curry3(L.foldl);
-export const reduce = foldl;
-export const foldr = curry3(L.foldr);
-export const reduceRight = foldr;
-export const update = curry3(L.update);
-export const adjust = curry3(L.adjust);
-export const slice = curry3(L.slice);
-export const remove = curry3(L.remove);
-export const insert = curry3(L.insert);
-export const insertAll = curry3(L.insertAll);
+
+export const foldl: typeof L.foldl & {
+  <A, B>(f: (acc: B, value: A) => B): Curried2<B, List<A>, B>;
+  <A, B>(f: (acc: B, value: A) => B, initial: B): (l: List<A>) => B;
+} = curry3(L.foldl);
+
+export const reduce: typeof foldl = foldl;
+
+export const foldr: typeof L.foldl & {
+  <A, B>(f: (value: A, acc: B) => B): Curried2<B, List<A>, B>;
+  <A, B>(f: (value: A, acc: B) => B, initial: B): (l: List<A>) => B;
+} = curry3(L.foldr);
+
+export const reduceRight: typeof foldr = foldr;
+
+export const update: typeof L.update & {
+  <A>(index: number, a: A): (l: List<A>) => List<A>;
+  <A>(index: number): ((a: A, l: List<A>) => List<A>) &
+    ((a: A) => (l: List<A>) => List<A>);
+} = curry3(L.update);
+
+export const adjust: typeof L.adjust & {
+  <A>(f: (value: A) => A, index: number): (l: List<A>) => List<A>;
+  <A>(f: (value: A) => A): Curried2<number, List<A>, List<A>>;
+} = curry3(L.adjust);
+
+export const slice: typeof L.slice & {
+  (from: number): (<A>(to: number) => (l: List<A>) => List<A>) &
+    (<A>(to: number, l: List<A>) => List<A>);
+  (from: number, to: number): <A>(l: List<A>) => List<A>;
+} = curry3(L.slice);
+
+export const remove: typeof slice = curry3(L.remove);
+
+export const insert: typeof update = curry3(L.insert);
+
+export const insertAll: typeof L.insertAll & {
+  <A>(index: number, elements: List<A>): (l: List<A>) => List<A>;
+  <A>(index: number): ((elements: List<A>, l: List<A>) => List<A>) &
+    ((elements: List<A>) => (l: List<A>) => List<A>);
+} = curry3(L.insertAll);
--- test/curried.ts
+++ test/curried.ts
@@ -24,4 +24,41 @@ describe("curried", () => {
     const l = Lc.map((n: number) => n * n)(Lc.list(1, 2, 3, 4));
     assert.isTrue(Lc.equals(l, Lc.list(1, 4, 9, 16)));
   });
+  it("partially applied nth can be called on lists of different types", () => {
+    const snd = Lc.nth(1);
+    const one = snd(Lc.list(0, 1, 2, 3));
+    assert.strictEqual(one, 1);
+    const b = snd(Lc.list("a", "b", "c", "d"));
+    assert.strictEqual(b, "b");
+  });
+  it("returns pluck with correct type", () => {
+    const pluckFoo = Lc.pluck("foo");
+    const l = pluckFoo(Lc.list({ foo: 0 }, { foo: 1 }, { foo: 2 }));
+    assert.isTrue(Lc.equals(l, Lc.list(0, 1, 2)));
+    // Should not give type error since `l` is list of `number`
+    assert.equal(Lc.foldl((a: number, b: number) => a + b, 0, l), 3);
+  });
+  it("can call foldl in all combinations", () => {
+    const f = (sum: number, s: string) => sum + s.length;
+    const l = Lc.list("hi ", "there");
+    const r1 = Lc.foldl(f, 0, l);
+    const r2 = Lc.foldl(f)(0, l);
+    const r4 = Lc.foldl(f, 0)(l);
+    const r3 = Lc.foldl(f)(0)(l);
+    assert.strictEqual(r1, 3 + 5);
+    assert.strictEqual(r2, 3 + 5);
+    assert.strictEqual(r3, 3 + 5);
+    assert.strictEqual(r4, 3 + 5);
+  });
+  it("can call slice in all combinations", () => {
+    const l = Lc.list(0, 1, 2, 3, 4, 5);
+    const l1 = Lc.slice(1, 5, l);
+    const l2 = Lc.slice(1)(5)(l);
+    const l3 = Lc.slice(1, 5)(l);
+    const l4 = Lc.slice(1)(5, l);
+    assert.isTrue(Lc.equals(l1, Lc.list(1, 2, 3, 4)));
+    assert.isTrue(Lc.equals(l2, Lc.list(1, 2, 3, 4)));
+    assert.isTrue(Lc.equals(l3, Lc.list(1, 2, 3, 4)));
+    assert.isTrue(Lc.equals(l4, Lc.list(1, 2, 3, 4)));
+  });
 });
