3ba9cc82f0a27231c3fa9f36f6e686f741332a2c
Simon Friis Vindum <simonfv@gmail.com>
2018-06-15 10:50:00 +0200

    Remove PureScript from main repository

--- purescript/test/Main.purs
+++
@@ -1,9 +0,0 @@
-module Test.Main where
-
-import Prelude
-import Control.Monad.Eff (Eff)
-import Control.Monad.Eff.Console (CONSOLE, log)
-
-main :: forall e. Eff (console :: CONSOLE | e) Unit
-main = do
-  log "You should add some tests."
